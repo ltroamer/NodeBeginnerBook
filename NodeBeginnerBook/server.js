409eff9f05f166bd89cf32a004e1afe6b972c951
[Tuan Le] <[lt.roamer@gmail.com]>
2018-09-03 03:51:45 +0700

    How function passing makes our HTTP server work

--- NodeBeginnerBook/server.js
+++ NodeBeginnerBook/server.js
@@ -1,7 +1,9 @@
 var http = require("http");
 
-http.createServer(function (request, response) {
+http.createServer(onRequest).listen(8888)
+
+function onRequest(request, response) {
     response.writeHead(200, { "Content-type": "text/plain" });
-    response.write("Hello World");
+    response.write("Hello");
     response.end();
-}).listen(8888)
+}
