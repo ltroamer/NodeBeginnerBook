90a78889c8f37b781f7546296cdd9ab7c08edbaa
[Tuan Le] <[lt.roamer@gmail.com]>
2018-09-03 03:43:19 +0700

    A basic HTTP server

--- NodeBeginnerBook/server.js
+++ NodeBeginnerBook/server.js
@@ -1,8 +1,7 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+var http = require("http");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-type": "text/plain" });
+    response.write("Hello World");
+    response.end();
+}).listen(8888)
